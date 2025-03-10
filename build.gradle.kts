2c747260ff4236cf9235e9199c235fc88bcb0df6
Daniil <bombyascher@gmail.com>
2025-03-10 19:36:01 +0300

    Define project structure and implement dependencies.

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    application
+    java
+    jacoco
+    checkstyle
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "io.hexlet"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+}
+
+testing {
+    suites {
+        // Configure the built-in test suite
+        val test by getting(JvmTestSuite::class) {
+            // Use JUnit Jupiter test framework
+            useJUnitJupiter("5.10.0")
+        }
+    }
+}
+
+// Apply a specific Java toolchain to ease working on different environments.
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+application {
+    // Define the main class for the application.
+    mainClass.set("io.hexlet.App")
+}
