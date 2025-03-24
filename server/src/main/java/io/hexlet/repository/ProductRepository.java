95fc0c340ec6b2bfacd856a69d41d18edc74e259
Rata0 <nekon689@gmail.com>
2025-03-24 15:10:55 +0300

    Added repositories for Cart, Favorite, Product, and User

---
+++ server/src/main/java/io/hexlet/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package io.hexlet.repository;
+
+import io.hexlet.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer>, JpaSpecificationExecutor<Product> {
+}
