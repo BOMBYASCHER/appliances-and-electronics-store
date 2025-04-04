d64e50c9bd89a37521908bb3cacadca7d2b5e3ee
Rata0 <nekon689@gmail.com>
2025-04-04 15:19:38 +0300

    Added annotation to provide spoofed user authentication

--- server/src/test/java/io/hexlet/api/ProductControllerTest.java
+++ server/src/test/java/io/hexlet/api/ProductControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@WithMockUser
 public class ProductControllerTest {
 
     private static final String DATE_PATTERN = "\\d{2}-\\d{2}-\\d{4}";
