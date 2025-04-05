9b114c8638513dd1ba65e950e59048dcd4386470
Rata0 <nekon689@gmail.com>
2025-04-05 18:30:05 +0300

    Renamed UserController-> AuthController, added login endpoint

--- server/src/main/java/io/hexlet/controller/AuthController.java
+++ server/src/main/java/io/hexlet/controller/AuthController.java
@@ -2,7 +2,7 @@ package io.hexlet.controller;
 
 import io.hexlet.dto.RegistrationDTO;
 import io.hexlet.model.User;
-import io.hexlet.service.UserService;
+import io.hexlet.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,13 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
-public class UserController {
+public class AuthController {
 
     @Autowired
-    private UserService userService;
+    private AuthService authService;
 
     @PostMapping("/registration")
     public User register(@RequestBody RegistrationDTO registrationDTO) {
-        return userService.register(registrationDTO);
+        return authService.register(registrationDTO);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody RegistrationDTO registrationDTO) {
+        return authService.authenticateAndGetToken(registrationDTO);
     }
 }
