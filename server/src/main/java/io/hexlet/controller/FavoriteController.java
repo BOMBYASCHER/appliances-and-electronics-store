5e2a8ba258afea4109ed25fdd1dc5137f9f80d1f
Rata0 <nekon689@gmail.com>
2025-04-06 16:41:57 +0300

    Add POST /favorites endpoint

--- server/src/main/java/io/hexlet/controller/FavoriteController.java
+++ server/src/main/java/io/hexlet/controller/FavoriteController.java
@@ -1,10 +1,16 @@
 package io.hexlet.controller;
 
+import io.hexlet.dto.AddToFavoritesRequestDTO;
 import io.hexlet.dto.FavoriteDTO;
+import io.hexlet.model.CustomUserDetails;
 import io.hexlet.service.FavoriteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +19,24 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/data")
 public class FavoriteController {
+
     @Autowired
     private FavoriteService favoriteService;
 
     @GetMapping("/favorites")
-    public ResponseEntity<List<FavoriteDTO>> index() {
-        List<FavoriteDTO> favorites = favoriteService.getAllFavoriteProducts();
+    public ResponseEntity<List<FavoriteDTO>> index(@AuthenticationPrincipal CustomUserDetails user) {
+        Integer userId = user.getUserId();
+        List<FavoriteDTO> favorites = favoriteService.getUserFavorites(userId);
+
         return ResponseEntity.ok(favorites);
+    }
+
+    @PostMapping("/favorites")
+    public ResponseEntity<Void> add(@AuthenticationPrincipal CustomUserDetails user,
+                                    @RequestBody AddToFavoritesRequestDTO request) {
+        Integer userId = user.getUserId();
+        favoriteService.addProductToFavorites(userId, request.getProductId());
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
