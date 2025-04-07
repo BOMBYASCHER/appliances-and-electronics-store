6c3dda7c0561105305e8c7004b09eeaf6b354203
Rata0 <nekon689@gmail.com>
2025-04-07 14:01:59 +0300

    Add POST endpoint for adding products to cart

--- server/src/main/java/io/hexlet/controller/CartController.java
+++ server/src/main/java/io/hexlet/controller/CartController.java
@@ -1,22 +1,30 @@
 package io.hexlet.controller;
 
-import io.hexlet.dto.CartDTO;
+import io.hexlet.dto.AddToCartsRequestDTO;
+import io.hexlet.model.CustomUserDetails;
 import io.hexlet.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/data")
 public class CartController {
+
     @Autowired
     private CartService cartService;
 
-    @GetMapping("/cart")
-    public ResponseEntity<CartDTO> index(@RequestHeader("X-Cart-Id") Integer cartId) {
-        return ResponseEntity.ok(cartService.getCartProducts(cartId));
+    @PostMapping("/cart")
+    public ResponseEntity<Void> add(@AuthenticationPrincipal CustomUserDetails user,
+                                    @RequestBody AddToCartsRequestDTO request) {
+        Integer userId = user.getUserId();
+        cartService.addProductToCarts(userId, request.getProductId());
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
