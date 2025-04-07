74f0b0361522a2a886bfbc5db7b9d7624cf0ee01
Rata0 <nekon689@gmail.com>
2025-04-07 13:58:49 +0300

    Create method for adding items to cart

--- server/src/main/java/io/hexlet/service/CartService.java
+++ server/src/main/java/io/hexlet/service/CartService.java
@@ -1,42 +1,35 @@
 package io.hexlet.service;
 
-import io.hexlet.dto.CartDTO;
-import io.hexlet.mapper.CartMapper;
-import io.hexlet.model.Product;
-import io.hexlet.repository.CartRepository;
+import io.hexlet.model.Cart;
+import io.hexlet.model.User;
 import io.hexlet.repository.ProductRepository;
+import io.hexlet.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 @Service
 public class CartService {
-    @Autowired
-    private CartRepository cartRepository;
+
     @Autowired
     private ProductRepository productRepository;
-    @Autowired
-    private CartMapper cartMapper;
 
-    public CartDTO getCartProducts(Integer cartId) {
-        if (!cartRepository.existsById(cartId)) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Cart with ID: " + cartId + "not found");
+    @Autowired
+    private UserRepository userRepository;
+
+    public void addProductToCarts(Integer userId, Integer productId) {
+        if (!productRepository.existsById(productId)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product Not Found");
         }
 
-        List<Integer> productIds = cartRepository.findProductIdsByCartId(cartId);
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("User Not Found"));
 
-        Map<Integer, Long> productQuantities = productIds.stream()
-                .collect(Collectors.groupingBy(id -> id, Collectors.counting()));
+        Cart cart = user.getCart();
 
-        List<Integer> uniqueProductIds = new ArrayList<>(productQuantities.keySet());
-        List<Product> products = productRepository.findAllById(uniqueProductIds);
-
-        return cartMapper.toCartDto(products, productQuantities);
+        cart.getProductIds().add(productId);
+        userRepository.save(user);
     }
 }
