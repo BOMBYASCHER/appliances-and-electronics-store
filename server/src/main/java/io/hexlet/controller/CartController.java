88c0c853ad038704b03baf3fd15397099b388e42
Rata0 <nekon689@gmail.com>
2025-04-07 16:13:14 +0300

    Add user cart retrieval endpoint in controller

--- server/src/main/java/io/hexlet/controller/CartController.java
+++ server/src/main/java/io/hexlet/controller/CartController.java
@@ -1,12 +1,14 @@
 package io.hexlet.controller;
 
 import io.hexlet.dto.AddToCartsRequestDTO;
+import io.hexlet.dto.CartDTO;
 import io.hexlet.model.CustomUserDetails;
 import io.hexlet.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,14 @@ public class CartController {
 
     @Autowired
     private CartService cartService;
+
+    @GetMapping("/cart")
+    public ResponseEntity<CartDTO> index(@AuthenticationPrincipal CustomUserDetails user) {
+        Integer userId = user.getUserId();
+        CartDTO cart = cartService.getUserCart(userId);
+
+        return ResponseEntity.ok(cart);
+    }
 
     @PostMapping("/cart")
     public ResponseEntity<Void> add(@AuthenticationPrincipal CustomUserDetails user,
