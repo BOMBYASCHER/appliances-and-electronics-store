d173323711f1dcc45f57e19a790301df1ffa4f5f
Rata0 <nekon689@gmail.com>
2025-04-07 16:20:58 +0300

    Create method to get user cart

--- server/src/main/java/io/hexlet/service/CartService.java
+++ server/src/main/java/io/hexlet/service/CartService.java
@@ -1,6 +1,10 @@
 package io.hexlet.service;
 
+import io.hexlet.dto.CartDTO;
+import io.hexlet.dto.CartItemDTO;
+import io.hexlet.mapper.CartMapper;
 import io.hexlet.model.Cart;
+import io.hexlet.model.Product;
 import io.hexlet.model.User;
 import io.hexlet.repository.ProductRepository;
 import io.hexlet.repository.UserRepository;
@@ -10,6 +14,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 @Service
 public class CartService {
 
@@ -18,6 +28,9 @@ public class CartService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private CartMapper cartMapper;
 
     public void addProductToCarts(Integer userId, Integer productId) {
         if (!productRepository.existsById(productId)) {
@@ -31,5 +44,33 @@ public class CartService {
 
         cart.getProductIds().add(productId);
         userRepository.save(user);
+    }
+
+    public CartDTO getUserCart(Integer userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("User Not Found"));
+
+        Cart cart = user.getCart();
+
+        if (cart == null || cart.getProductIds().isEmpty()) {
+            return cartMapper.emptyCart();
+        }
+
+        Map<Integer, Integer> quantities = cart.getProductIds().stream()
+                .collect(Collectors.toMap(Function.identity(), id -> 1, Integer::sum));
+
+        List<Product> products = productRepository.findAllById(quantities.keySet());
+
+        List<CartItemDTO> items = new ArrayList<>();
+        int totalAmount = 0;
+
+        for (Product product : products) {
+            int quantity = quantities.get(product.getId());
+            CartItemDTO itemDto = cartMapper.toCartItemDto(new CartItemDTO(), product, quantity);
+            items.add(itemDto);
+            totalAmount += product.getPrice() * quantity;
+        }
+
+        return cartMapper.toCartDto(totalAmount, items);
     }
 }
