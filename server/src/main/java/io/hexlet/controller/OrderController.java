13ba5a61e5dcb62e45fa65a82a802cd80fd3caab
Rata0 <nekon689@gmail.com>
2025-04-09 21:48:01 +0300

    Added a GET /orders endpoint to retrieve user orders

--- server/src/main/java/io/hexlet/controller/OrderController.java
+++ server/src/main/java/io/hexlet/controller/OrderController.java
@@ -1,5 +1,6 @@
 package io.hexlet.controller;
 
+import io.hexlet.dto.OrderDTO;
 import io.hexlet.dto.OrderItemRequestDTO;
 import io.hexlet.model.CustomUserDetails;
 import io.hexlet.service.OrderService;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,14 @@ public class OrderController {
 
     @Autowired
     private OrderService orderService;
+
+    @GetMapping("/orders")
+    public ResponseEntity<List<OrderDTO>> index(@AuthenticationPrincipal CustomUserDetails user) {
+        Integer userId = user.getUserId();
+        List<OrderDTO> orders = orderService.getUserOrders(userId);
+
+        return ResponseEntity.ok(orders);
+    }
 
     @PostMapping("/orders")
     public ResponseEntity<Void> create(@AuthenticationPrincipal CustomUserDetails user,
