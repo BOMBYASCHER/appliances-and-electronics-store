4a15fa1bbd62a9b82675d1782b7068dd43ed075d
Rata0 <nekon689@gmail.com>
2025-04-09 21:26:15 +0300

    Added method for getting orders

--- server/src/main/java/io/hexlet/repository/OrderRepository.java
+++ server/src/main/java/io/hexlet/repository/OrderRepository.java
@@ -1,7 +1,11 @@
 package io.hexlet.repository;
 
 import io.hexlet.model.Order;
+import io.hexlet.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+    List<Order> findByUser(User user);
 }
