6593dbe57634fef61b81cbac0d73b6f41723f420
Rata0 <nekon689@gmail.com>
2025-04-10 13:41:05 +0300

    Add methods to find Purchase by id and orderId

--- server/src/main/java/io/hexlet/repository/PurchaseRepository.java
+++ server/src/main/java/io/hexlet/repository/PurchaseRepository.java
@@ -3,5 +3,9 @@ package io.hexlet.repository;
 import io.hexlet.model.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PurchaseRepository extends JpaRepository<Purchase, Integer> {
+    Optional<Purchase> findById(Integer id);
+    Optional<Purchase> findByIdAndOrderId(Integer purchaseId, Integer orderId);
 }
