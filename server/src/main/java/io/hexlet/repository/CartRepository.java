8e228cfaca8204aba934aa4ab9fba965e81e9036
Rata0 <nekon689@gmail.com>
2025-04-14 14:17:08 +0300

    Deleting unused methods

--- server/src/main/java/io/hexlet/repository/CartRepository.java
+++ server/src/main/java/io/hexlet/repository/CartRepository.java
@@ -2,14 +2,8 @@ package io.hexlet.repository;
 
 import io.hexlet.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Integer> {
-    @Query(value = "SELECT product_id FROM cart_products WHERE cart_id = :cartId", nativeQuery = true)
-    List<Integer> findProductIdsByCartId(@Param("cartId") Integer cartId);
 }
--- server/src/main/java/io/hexlet/repository/FavoriteRepository.java
+++ server/src/main/java/io/hexlet/repository/FavoriteRepository.java
@@ -1,16 +1,9 @@
 package io.hexlet.repository;
 
 import io.hexlet.model.Favorite;
-import io.hexlet.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface FavoriteRepository extends JpaRepository<Favorite, Integer> {
-    @Query("SELECT DISTINCT product FROM Product product WHERE product.id IN "
-           + "(SELECT f FROM Favorite favorite JOIN favorite.productIds f)")
-    List<Product> findAllFavoriteProducts();
 }
