721f287ec07f5e28ca88c3edf1df13d06590240a
Rata0 <nekon689@gmail.com>
2025-04-18 13:15:37 +0300

    Rewrote the code using ReturnMapper

--- server/src/main/java/io/hexlet/service/ReturnService.java
+++ server/src/main/java/io/hexlet/service/ReturnService.java
@@ -4,15 +4,16 @@ import io.hexlet.dto.ReturnDTO;
 import io.hexlet.dto.ReturnRequestDTO;
 import io.hexlet.exception.AccessDeniedException;
 import io.hexlet.exception.ResourceNotFoundException;
+import io.hexlet.mapper.ReturnMapper;
 import io.hexlet.model.Purchase;
 import io.hexlet.model.Return;
 import io.hexlet.model.User;
 import io.hexlet.repository.PurchaseRepository;
 import io.hexlet.repository.ReturnRepository;
 import io.hexlet.repository.UserRepository;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,54 +30,37 @@ public class ReturnService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private ReturnMapper returnMapper;
+
+    @Transactional
     public void createReturn(int userId, ReturnRequestDTO request) {
         User user = getUserOrThrow(userId);
 
         Purchase purchase = purchaseRepository.findByIdAndOrderId(request.getPurchaseId(), request.getOrderId())
-                .orElseThrow(() -> new EntityNotFoundException("Purchase not found in this order"));
+                .orElseThrow(() -> new ResourceNotFoundException("Purchase not found in this order"));
 
         if (purchase.getOrder().getUser().getId() != userId) {
             throw new AccessDeniedException("You are not allowed to create return for this purchase");
         }
 
-        Return returnEntity = new Return();
-        returnEntity.setPurchaseId(request.getPurchaseId());
-        returnEntity.setReason(request.getReason());
-        returnEntity.setPhoto(request.getPhoto());
-        returnEntity.setUser(user);
-        returnEntity.setDate(null);
+        Return returnEntity = returnMapper.toReturnEntity(request, user);
 
         returnRepository.save(returnEntity);
     }
 
     public List<ReturnDTO> getReturnsForUser(int userId) {
         User user = getUserOrThrow(userId);
-
         List<Return> returns = returnRepository.findByUser(user);
+
         return returns.stream()
-                .map(this::convertToDTO)
+                .map(returnItem -> {
+                    Purchase purchase = purchaseRepository.findById(returnItem.getPurchaseId())
+                            .orElseThrow(() -> new ResourceNotFoundException("Purchase not found"));
+
+                    return returnMapper.toReturnDTO(returnItem, purchase);
+                })
                 .collect(Collectors.toList());
-    }
-
-    private ReturnDTO convertToDTO(Return returnItem) {
-        Purchase purchase = purchaseRepository.findById(returnItem.getPurchaseId())
-                .orElseThrow(() -> new EntityNotFoundException("Purchase not found"));
-
-        ReturnDTO dto = new ReturnDTO();
-        dto.setId(returnItem.getId());
-        dto.setOrderId(purchase.getOrder().getId());
-        dto.setProductId(purchase.getProduct().getId());
-        dto.setOrderTitle(purchase.getOrder().getTitle());
-        dto.setProductTitle(purchase.getProductTitle());
-        dto.setImage(purchase.getProductImage());
-        dto.setTotalAmount(purchase.getOrder().getTotalAmount());
-        dto.setPrice(purchase.getProductPrice());
-        dto.setQuantity(purchase.getQuantity());
-        dto.setDate(null);
-        dto.setReason(returnItem.getReason());
-        dto.setPhoto(returnItem.getPhoto());
-
-        return dto;
     }
 
     public User getUserOrThrow(Integer userId) {
