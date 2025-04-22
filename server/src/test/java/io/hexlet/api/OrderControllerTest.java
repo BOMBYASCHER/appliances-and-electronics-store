0f6f5de379fdde1a0238a3916dfbf8cf999765f0
Rata0 <nekon689@gmail.com>
2025-04-22 14:02:56 +0300

    Fixed tests for products

--- server/src/test/java/io/hexlet/api/OrderControllerTest.java
+++ server/src/test/java/io/hexlet/api/OrderControllerTest.java
@@ -20,7 +20,9 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
