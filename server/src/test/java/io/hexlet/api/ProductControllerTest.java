46f26f48a8122709113b0ed411b748fcdb5dacad
Rata0 <nekon689@gmail.com>
2025-04-22 12:55:50 +0300

    Added Testcontainers

--- server/src/test/java/io/hexlet/api/ProductControllerTest.java
+++ server/src/test/java/io/hexlet/api/ProductControllerTest.java
@@ -10,7 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -19,9 +21,11 @@ import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WithMockUser
 @SpringBootTest
 @AutoConfigureMockMvc
-@WithMockUser
+@ActiveProfiles("test")
+@Import(TestcontainersConfiguration.class)
 public class ProductControllerTest {
 
     private static final String BASE_API_PATH = "/api/data";
