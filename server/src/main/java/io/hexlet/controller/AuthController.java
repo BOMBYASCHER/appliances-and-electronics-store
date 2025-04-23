db0f3369c2419d4f9ffe178971c5fb724e2ee202
Rata0 <nekon689@gmail.com>
2025-04-23 16:33:12 +0300

    Refactor registration and login endpoints: add response entities and validation

--- server/src/main/java/io/hexlet/controller/AuthController.java
+++ server/src/main/java/io/hexlet/controller/AuthController.java
@@ -1,9 +1,13 @@
 package io.hexlet.controller;
 
+import io.hexlet.dto.LoginDTO;
 import io.hexlet.dto.RegistrationDTO;
 import io.hexlet.model.User;
 import io.hexlet.service.AuthService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +21,14 @@ public class AuthController {
     private AuthService authService;
 
     @PostMapping("/registration")
-    public User register(@RequestBody RegistrationDTO registrationDTO) {
-        return authService.register(registrationDTO);
+    public ResponseEntity<User> register(@RequestBody @Valid RegistrationDTO registrationDTO) {
+        User user = authService.register(registrationDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody RegistrationDTO registrationDTO) {
-        return authService.authenticateAndGetToken(registrationDTO);
+    public ResponseEntity<String> login(@RequestBody LoginDTO loginDTO) {
+        var body = authService.authenticateAndGetToken(loginDTO);
+        return ResponseEntity.ok(body);
     }
 }
