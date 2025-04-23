ca7c06dcd41d1a3ec60d39626edfa52b292ab798
Rata0 <nekon689@gmail.com>
2025-04-23 16:08:09 +0300

    Added method to check phone in database

--- server/src/main/java/io/hexlet/repository/UserRepository.java
+++ server/src/main/java/io/hexlet/repository/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByPhone(String phone);
+    boolean existsByPhone(String phone);
 }
