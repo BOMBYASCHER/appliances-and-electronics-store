8bc8cfbce4c5260713a33f691ff4f464d0ce1bda
Rata0 <nekon689@gmail.com>
2025-05-12 12:42:12 +0300

    Add automatic order status updates every 10 seconds

--- server/src/main/java/io/hexlet/model/enums/converter/OrderStatusConverter.java
+++ server/src/main/java/io/hexlet/model/enums/converter/OrderStatusConverter.java
@@ -7,12 +7,12 @@ import jakarta.persistence.Converter;
 @Converter(autoApply = true)
 public class OrderStatusConverter implements AttributeConverter<OrderStatus, String> {
     @Override
-    public String convertToDatabaseColumn(OrderStatus status) {
-        return status == null ? null : status.name();
+    public String convertToDatabaseColumn(OrderStatus attribute) {
+        return attribute != null ? attribute.name() : null;
     }
 
     @Override
-    public OrderStatus convertToEntityAttribute(String dbValue) {
-        return dbValue == null ? null : OrderStatus.fromString(dbValue);
+    public OrderStatus convertToEntityAttribute(String dbData) {
+        return dbData != null ? OrderStatus.valueOf(dbData) : null;
     }
 }
