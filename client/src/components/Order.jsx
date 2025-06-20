cbb909fd29245e6a574ff013eb285b9f74727fd8
Daniil <bombyascher@gmail.com>
2025-06-20 17:55:55 +0300

    Design implementation.

--- client/src/components/Order.jsx
+++ client/src/components/Order.jsx
@@ -1,4 +1,3 @@
-import cn from 'classnames';
 import { useState } from "react";
 import { Link } from 'react-router';
 
@@ -6,55 +5,38 @@ const Order = ({ id: orderId, title: orderTitle, totalAmount, date, purchases, s
   const [isShown, setIsShown] = useState(false);
   
   const toggleAccordion = () => {
+    console.log('click')
+    console.log(isShown)
     setIsShown(!isShown);
   };
 
-  const accordionBtnClass = cn('accordion-button', {
-    collapsed: !isShown,
-  });
-  const accordionClass = cn('accordion-collapse collapse', {
-    show: isShown,
-  });
-
   return (
-    <div key={orderId} className="row-md-6">
-    <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-      <div className="col p-4 d-flex flex-column position-static">
-        <h4 className="pb-3 mb-0">{orderTitle}</h4>
-        <p className="fs-5 mb-auto">Общая сумма: <strong>{totalAmount}</strong></p>
-        <p className="fs-5 mb-auto">Дата заказа: <strong>{new Date(date).toLocaleDateString()}</strong></p>
-        <p className="fs-5 mb-auto">Статус: <strong>{status}</strong></p>
+    <div key={orderId} className="order-container">
+      <div className="order-header">
+        <h1>{orderTitle}</h1>
+        <p>Общая сумма: {totalAmount}</p>
+        <p>Дата заказа: {new Date(date).toLocaleDateString()}</p>
+        <p>Статус: {status}</p>
       </div>
-      <div className="accordion" id="accordionExample">
-      <div className="accordion-item">
-        <h2 className="accordion-header">
-          <button onClick={toggleAccordion} className={accordionBtnClass} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded={isShown} aria-controls="collapseOne">
-            Покупки
-          </button>
-        </h2>
-        <div id="collapseOne" className={accordionClass} data-bs-parent="#accordionExample">
-          <div className="accordion-body">
-            <div className='row row-cols-sm-3 row-cols-md-6 g-3'>
-              {purchases.map(({ id, productId, title, price, image, quantity, isReturned }) => {
-                const formInfo = { orderId, purchaseId: id, title, price, image, quantity, date, orderTitle };
-                return (
-                  <Purchase
-                    key={id}
-                    title={title}
-                    price={price}
-                    image={image}
-                    quantity={quantity}
-                    formInfo={formInfo}
-                    isReturned={isReturned}
-                  />
-                );
-              })}
-            </div>
-          </div>
-        </div>
+      <div className="purchase-header" onClick={toggleAccordion}>
+        Покупки
       </div>
+      <div className="purchase-content" style={{ display: isShown ? 'block' : 'none' }}>
+        {purchases.map(({ id, productId, title, price, image, quantity, isReturned }) => {
+          const formInfo = { orderId, purchaseId: id, title, price, image, quantity, date, orderTitle };
+          return (
+            <Purchase
+              key={id}
+              title={title}
+              price={price}
+              image={image}
+              quantity={quantity}
+              formInfo={formInfo}
+              isReturned={isReturned}
+            />
+          );
+        })}
       </div>
-    </div>
     </div>
   );
 };
@@ -63,22 +45,15 @@ const Purchase = ({ title, price, image, quantity, formInfo, isReturned }) => {
   const returnBlock = isReturned ?
     <p>Покупка возвращена</p> :
     <Link state={formInfo} to='/return-form'>
-      <button disabled={isReturned} className='btn btn-primary'>Вернуть</button>
+      <button disabled={isReturned} className='return-button'>Вернуть</button>
     </Link>
   return (
-    <div className="col">
-      <div className='card shadow-sm'>
-        <div className='card-body'>
-          <p className="fs-5">{title}</p>
-          <div className='col-auto d-none d-lg-block'>
-            <img width={'auto'} height={100} alt="Product image" src={image} />
-          </div>
-          <div className="d-flex justify-content-between align-items-center">
-            <p className="card-text fw-light">Количество: {quantity}</p>
-            <p className="">Цена: {price}</p>
-          </div>
-          {returnBlock}
-        </div>
+    <div className='purchase-details'>
+      <img alt="Product image" src={image} />
+      <div>
+        <p><strong>{title}</strong></p>
+        <p>Количество: {quantity} Цена: {price}</p>
+        {returnBlock}
       </div>
     </div>
   );
--- client/src/pages/Orders.jsx
+++ client/src/pages/Orders.jsx
@@ -2,54 +2,56 @@ import { Link } from "react-router";
 import Header from "../components/Header";
 import Order from "../components/Order";
 import { useGetOrdersQuery } from "../slices/api/ordersApi";
+import style from '../assets/style/Orders.css?inline';
+import Footer from "../components/Footer";
 
 const Orders = () => {
   const { data: orders = [] } = useGetOrdersQuery(null, { pollingInterval: 1000, skipPollingIfUnfocused: true });
 
   return (
     <>
+    <style type='text/css'>{style}</style>
     <Header/>
-    <div className='container'>
-      {
-        orders.length == 0 ?
-        <EmptyHero/> :
-        <>
-        <h1 className='pb-4'>Ваши заказы</h1>
-        {orders.map(({ id, title, totalAmount, date, purchases, status }) => {
-          return (<Order
-            id={id}
-            title={title}
-            totalAmount={totalAmount}
-            date={date}
-            purchases={purchases}
-            status={status}
-          />)
-        })}
-        </>
-      }
-    </div>
+    {
+      orders.length == 0 ?
+      <EmptyHero/> :
+      <>
+      {orders.map(({ id, title, totalAmount, date, purchases, status }) => {
+        return (<Order
+          id={id}
+          title={title}
+          totalAmount={totalAmount}
+          date={date}
+          purchases={purchases}
+          status={status}
+        />)
+      })}
+      </>
+    }
+    <Footer/>
     </>
   );
 };
 
 const EmptyHero = () => {
   return (
-    <div class="px-4 py-5 my-5 text-center">
-      <h1 class="display-5 fw-bold">Заказов нет.</h1>
-      <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">
-        Вы ещё ничего не заказывали. Можете оформить заказ на странице корзины или посмотреть каталог.
-      </p>
-      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-        <Link to='/cart'>
-          <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Корзина</button>
-        </Link>
-        <Link to='/'>
-          <button type="button" class="btn btn-outline-secondary btn-lg px-4">Каталог</button>
-        </Link>
-      </div>
-      </div>
-    </div>
+    <></>
+    // <div class="px-4 py-5 my-5 text-center">
+    //   <h1 class="display-5 fw-bold">Заказов нет.</h1>
+    //   <div class="col-lg-6 mx-auto">
+    //   <p class="lead mb-4">
+    //     Вы ещё ничего не заказывали. Можете оформить заказ на странице корзины или посмотреть каталог.
+    //   </p>
+    //   <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+    //     <Link to='/cart'>
+    //       <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Корзина</button>
+    //     </Link>
+    //     <Link to='/'>
+    //       <button type="button" class="btn btn-outline-secondary btn-lg px-4">Каталог</button>
+    //     </Link>
+    //   </div>
+    //   </div>
+    // </div>
   );
 };
 
